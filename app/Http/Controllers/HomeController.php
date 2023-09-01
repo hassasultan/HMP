c33c919edad6d9b3fead082c389033459a393bf5
Hassan Sultan <h.hsultan277@gmail.com>
2023-09-01 17:12:12 +0500

    push

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -620,9 +620,11 @@ class HomeController extends Controller
     {
 
         $url =  route('vehicle.details',$id);
-        // dd($url);
+        $driver = Truck::with('truckCap','hydrant','drivers')->find($id);
+
+        dd($driver->toArray());
         // return \QrCode::size(300)->generate($url);
-        return view('pages.truck.qr',compact('url'));
+        return view('pages.truck.qr',compact('url','driver'));
     }
     public function vehicleDetails($id)
     {
