72bde8ca0bc20da6fcca83eb5bb452a222927e69
Hassan Sultan <h.hsultan277@gmail.com>
2023-09-12 21:24:41 +0500

    pushed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,32 +15,32 @@
 <link href="https://fonts.googleapis.com/css?family=Poppins:100,300,400,600,700,800,900" rel="stylesheet" type="text/css">
 
 <!-- Vendor CSS -->
-<link rel="stylesheet" href={{ asset('public/assets/vendor/bootstrap/css/bootstrap.css') }} />
-{{-- <link rel="stylesheet" href={{ asset('public/assets/vendor/animate/animate.compat.css') }} /> --}}
-{{-- <link rel="stylesheet" href={{ asset('public/assets/vendor/font-awesome/css/all.min.css') }} /> --}}
-<link rel="stylesheet" href={{ asset('public/assets/vendor/boxicons/css/boxicons.min.css') }} />
-{{-- <link rel="stylesheet" href={{ asset('public/assets/vendor/magnific-popup/magnific-popup.css') }} />
-<link rel="stylesheet" href={{ asset('public/assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker3.css') }} />
-<link rel="stylesheet" href={{ asset('public/assets/vendor/morris/morris.css') }} /> --}}
-<link rel="stylesheet" href={{ asset('public/assets/vendor/bootstrap-tagsinput/bootstrap-tagsinput.css') }} />
-{{-- <link rel="stylesheet" href={{ asset('public/assets/vendor/datatables/media/css/dataTables.bootstrap5.css') }} /> --}}
+<link rel="stylesheet" href={{ asset('/assets/vendor/bootstrap/css/bootstrap.css') }} />
+{{-- <link rel="stylesheet" href={{ asset('/assets/vendor/animate/animate.compat.css') }} /> --}}
+{{-- <link rel="stylesheet" href={{ asset('/assets/vendor/font-awesome/css/all.min.css') }} /> --}}
+<link rel="stylesheet" href={{ asset('/assets/vendor/boxicons/css/boxicons.min.css') }} />
+{{-- <link rel="stylesheet" href={{ asset('/assets/vendor/magnific-popup/magnific-popup.css') }} />
+<link rel="stylesheet" href={{ asset('/assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker3.css') }} />
+<link rel="stylesheet" href={{ asset('/assets/vendor/morris/morris.css') }} /> --}}
+<link rel="stylesheet" href={{ asset('/assets/vendor/bootstrap-tagsinput/bootstrap-tagsinput.css') }} />
+{{-- <link rel="stylesheet" href={{ asset('/assets/vendor/datatables/media/css/dataTables.bootstrap5.css') }} /> --}}
 
 <!-- Theme CSS -->
-<link rel="stylesheet" href={{ asset('public/assets/css/theme.css') }} />
-<link rel="stylesheet" type="text/css" href={{ asset('public/assets/css/vendors.css') }}  />
-<link rel="stylesheet" type="text/css" href={{ asset('public/assets/css/style.css') }}  />
+<link rel="stylesheet" href={{ asset('/assets/css/theme.css') }} />
+<link rel="stylesheet" type="text/css" href={{ asset('/assets/css/vendors.css') }}  />
+<link rel="stylesheet" type="text/css" href={{ asset('/assets/css/style.css') }}  />
 
 <!-- Theme Layout -->
-<link rel="stylesheet" href={{ asset('public/assets/css/layouts/modern.css') }} />
+<link rel="stylesheet" href={{ asset('/assets/css/layouts/modern.css') }} />
 
 <!-- Skin CSS -->
-<link rel="stylesheet" href={{ asset('public/assets/css/skins/default.css') }} />
+<link rel="stylesheet" href={{ asset('/assets/css/skins/default.css') }} />
 
 <!-- Theme Custom CSS -->
-<link rel="stylesheet" href={{ asset('public/assets/css/custom.css') }} />
+<link rel="stylesheet" href={{ asset('/assets/css/custom.css') }} />
 
 <!-- Head Libs -->
-<script src={{ asset('public/assets/vendor/modernizr/modernizr.js') }}></script>
+<script src={{ asset('/assets/vendor/modernizr/modernizr.js') }}></script>
 
 </head>
 <body style="background-image:url({{ asset('/assets/img/login-bg.jpeg') }});">
