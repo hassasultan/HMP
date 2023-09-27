1792a4efe61f7188b02fa5681bb7c8838b1dffc0
Hassan Sultan <h.hsultan277@gmail.com>
2023-09-28 01:27:38 +0500

    pushed

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/create/order',[App\Http\Controllers\OrderController::class,'create_ots_order']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('create/order',[App\Http\Controllers\OrderController::class,'create_ots_order']);
