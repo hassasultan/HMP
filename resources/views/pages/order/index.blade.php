50b3417605c488f100849fe0a5d85afc36cab6c7
Hassan Sultan <h.hsultan277@gmail.com>
2023-10-27 15:21:30 +0500

    pushed

--- resources/views/pages/order/index.blade.php
+++ resources/views/pages/order/index.blade.php
@@ -183,10 +183,9 @@
                                             </td>
 
                                             <td class="align-middle text-center">
-                                                @if ($row->billing != null)
+                                                @if ($row->billing != '[]')
                                                     <span class="badge bg-gradient-primary">Already generated</span>
                                                 @else
-                                                {{ dd($row->billing) }}
                                                     <a class="btn bg-gradient-dark mb-0 mr-3"
                                                         href="{{ route('billing.create', $row->id) }}"><i
                                                             class="material-icons text-sm">add</i>&nbsp;&nbsp;<i
