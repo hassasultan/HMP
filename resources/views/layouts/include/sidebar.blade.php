e119356435e17463775bde68a2881f4dfd63c477
hassasultan <h.hsultan277@gmail.com>
2024-03-09 20:03:55 +0500

    pusheed

--- resources/views/layouts/include/sidebar.blade.php
+++ resources/views/layouts/include/sidebar.blade.php
@@ -62,8 +62,8 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-white @if (Route::is('rtrack.truck.list')) active bg-gradient-primary @endif"
-                            href="{{ route('rtrack.truck.list') }}">
+                        <a class="nav-link text-white @if (Route::is('track.truck.list')) active bg-gradient-primary @endif"
+                            href="{{ route('track.truck.list') }}">
                             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                                 <i class="fas fa-user"></i>
                             </div>
