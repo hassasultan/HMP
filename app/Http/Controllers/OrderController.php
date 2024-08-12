b18d85f05fa9b05a193edea661f7d9091ee88b41
hassasultan <h.hsultan277@gmail.com>
2024-08-12 14:51:45 +0500

    pushed

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -43,7 +43,7 @@ class OrderController extends Controller
                 $order = $order->whereHas('customer');
                 $customer = Customer::where('standard', 'Commercial')->where('user_id',auth()->user()->id)->pluck('id');
                 $order = $order->whereIn('customer_id', $customer);
-                dd($customer->paginate('10')->toArray());
+                dd($order->paginate('10')->toArray());
                 // $order = $order->where('order_type', 'Commercial');
                 // dd(auth()->user()->id);
                 // $order = $order->orwhere('order_type', 'Commercial');
