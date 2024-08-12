e39236a7785efbcf929afe83c71ce64d4bfdab0b
hassasultan <h.hsultan277@gmail.com>
2024-08-12 23:16:08 +0500

    pushed

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -380,7 +380,7 @@ class OrderController extends Controller
             });
         }
         if ($request->has('from_date') && $request->from_date != '' && $request->has('to_date') && $request->to_date != '') {
-            $billing = $billing->whereBetween('created_at', [$request->from_date, $request->to_date]);
+            $billing = $billing->whereBetween('created_at', [$request->from_date.' 00:00:00', $request->to_date.' 00:00:00']);
         }
         // dd($order->OrderBy('id', 'DESC')->get()->toArray());
 
